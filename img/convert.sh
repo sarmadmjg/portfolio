8758dd5faf3918f73fdd4e407b3d2e666382192d
Sarmad Georgie <sarmadmjg@gmail.com>
2017-06-16 20:12:59 +0300

    Add banner images

---
+++ img/convert.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# F=$1
+# W=$2
+# H=$3
+
+
+magick banner.png -resize 1440 banner-1440.jpg
+magick banner.png -resize 1080 banner-1080.jpg
+magick banner.png -resize 720 banner-720.jpg
